3badf720098f8bfd01b8890021b04e30654f57a3
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-06-19 00:09:29 +0600

    setup media option in our project

--- App_shop/models.py
+++ App_shop/models.py
@@ -1,3 +1,8 @@
+from distutils.command.upload import upload
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=20)
+    category_img=models.ImageField(upload_to="shop/image/category/")
--- App_shop/urls.py
+++ App_shop/urls.py
@@ -1,4 +1,6 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +9,4 @@ from App_shop.views import Home
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
