6f5153f3f0656c4b15c679b0a505fcd930f7decb
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-06-18 23:14:16 +0600

    basic view

--- App_shop/views.py
+++ App_shop/views.py
@@ -1,3 +1,11 @@
 from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+
+class Home(View):
+    
+    def get(self, request):
+        return HttpResponse("shop home")
