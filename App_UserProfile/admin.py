4caeb37ca4d50d01a76161deca4269e622f95ada
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-08-29 23:57:03 +0600

    add user profile model

--- App_UserProfile/admin.py
+++ App_UserProfile/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from App_UserProfile.models import UserProfile
 # Register your models here.
+admin.site.register(UserProfile)
--- App_UserProfile/models.py
+++ App_UserProfile/models.py
@@ -1,3 +1,21 @@
+import email
+from email.headerregistry import Address
+from email.mime import image
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    email = models.EmailField(max_length=30, blank=False)
+    phone = models.CharField(max_length=15, blank=False)
+    image = models.ImageField(upload_to="UserProfile/", default='UserProfile/profile.png')
+    description = models.CharField(max_length=150, blank=True)
+    address = models.CharField(max_length=20, blank=True)
+    otp=models.CharField(max_length=25)
+
+
+    def __str__(self):
+        return self.user.username
