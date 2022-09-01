0bcfc3904879f0460d087fc5fb0e944904b47bbc
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-09-02 01:03:52 +0600

    setup smtp
    setup smtp

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,11 @@ MESSAGE_TAGS = {
     messages.WARNING: 'danger',
     messages.ERROR: 'danger',
 }
+
+# SMTP mail sending 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'motalibhossainrbk33@gmail.com'
+EMAIL_HOST_PASSWORD = 'jvtlyqgcyhthlile'
