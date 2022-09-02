c005d6d0e88033018395102542f7baca89351ec2
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-09-03 00:44:23 +0600

    Sending mail complete.
    Sending mail complete.

--- App_UserProfile/urls.py
+++ App_UserProfile/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 # Import views 
-from App_UserProfile.views import Userlogin, UserRegister, Profile
+from App_UserProfile.views import Userlogin, UserRegister, Profile, Verify
 
 app_name='App_UserProfile'
 urlpatterns = [
     path('user/', Profile, name ='profile' ),
     path('login/', Userlogin, name ='login' ),
     path('registration/', UserRegister, name ='UserRegister' ),
-    path('verify/<otp>', UserRegister, name ='verify' ),
+    path('verify/<otp>', Verify, name ='Verify' ),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
