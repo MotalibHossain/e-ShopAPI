41c6d55cd67c4dbd68c7b003d4b319181a0bf11a
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-09-03 21:14:33 +0600

    update
    update

--- App_UserProfile/urls.py
+++ App_UserProfile/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 # Import views 
-from App_UserProfile.views import Userlogin, UserRegister, Profile, Verify
+from App_UserProfile.views import Userlogin, UserRegister, Profile, Verify, logoutView, Profile
 
 app_name='App_UserProfile'
 urlpatterns = [
     path('user/', Profile, name ='profile' ),
-    path('login/', Userlogin, name ='login' ),
     path('registration/', UserRegister, name ='UserRegister' ),
+    path('login/', Userlogin, name ='login' ),
+    path('profile/', Profile, name ='Profile' ),
+    path('logout/', logoutView, name ='logoutView' ),
     path('verify/<otp>', Verify, name ='Verify' ),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
