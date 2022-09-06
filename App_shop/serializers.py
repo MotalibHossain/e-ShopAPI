7b53519ce14faf43810fab089fd3bb2a335f3130
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-09-07 00:01:50 +0600

    Implementing post method.
    Implementing post method.

--- App_shop/serializers.py
+++ App_shop/serializers.py
@@ -1,11 +1,16 @@
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 from App_shop.models import Category, Product
 
 class CategorySerializer(serializers.Serializer):
     id=serializers.CharField(read_only=True)
     name=serializers.CharField(max_length=10)
+    # category_img=Base64ImageField()
     category_img=serializers.ImageField()
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
 
 class ProductSerializer(serializers.Serializer):
     name=serializers.CharField()
